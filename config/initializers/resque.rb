b018d51ee0699560fea86a33c766bb6898a708e5
Kasvit <kasvit93@gmail.com>
2024-07-22 14:17:08 +0300

    fix resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,5 @@
 require 'resque'
 require 'resque/scheduler'
 
-Resque.redis = Redis.new(url: ENV['REDIS_URL'] || 'redis://redis:6379')
+Resque.redis = Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379')
 Resque.schedule = YAML.load_file(Rails.root.join('config', 'resque_schedule.yml'))
